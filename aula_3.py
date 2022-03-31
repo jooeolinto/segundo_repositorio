33b4107ea0f3258096ff7b724ba98bb941a9caf9
joeolinto <jooeolinto@gmail.com>
2022-03-31 11:27:15 -0300

    função subtrai criada

--- aula_3.py
+++ aula_3.py
@@ -1,5 +1,9 @@
 def divide(a, b):
     print(f"{a} dividido por {b} é igual a {a / b}.")
 
+def subtrai(a, b):
+    print(f"{a} subtraido por {b} é igual a {a - b}.")
+
 
 divide(10, 2)
+subtrai(7, 3)
